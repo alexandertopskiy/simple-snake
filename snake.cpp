ab9c678419181608a6939518331201e506e96a2d
Alexander Loshakov <alevas556@icloud.com>
2020-06-24 17:38:19 +0300

    Описание функции ввода

--- snake.cpp
+++ snake.cpp
@@ -8,11 +8,49 @@ int x, y, thingX, thingY, score;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 
+//функции для проверки буфера. т.к. в unix нет <conio.h> и встроенных в нее kbhit и getch
+int kbhit(void){
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+int mygetch( ){
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldt );
+    newt = oldt;
+    newt.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt );
+    return ch;
+}
+
 void Setup(){
 	gameover = false;
 	dir = STOP;
-	x = wigth / 2;
-	y = height / 2;
+	x = wigth / 2 - 1;
+	y = height / 2 - 1;
 	thingX = rand() % wigth;
 	thingY = rand() % height;
 	score = 0;
@@ -28,6 +66,10 @@ void Draw() {
 		for (int j = 0; j < wigth; j++) {
 			if (j==0 || j==wigth-1) 
 				cout << "#";
+			if (i == y && j == x)
+				cout << "0";
+			if (i == thingY && j == thingX)
+				cout << "F";
 			cout << " ";
 		}
 		cout << endl;
@@ -39,7 +81,27 @@ void Draw() {
 }
 
 void Input(){
+	if (kbhit()) key = mygetch();
+    if ((key == 'q') || (key == 'Q')) break;
+    switch(key) {
+    	case 'a':
+    		dir = LEFT;
+    		break;
+    	case 'd':
+    		dir = RIGHT;
+    		break;
+    	case 'w':
+    		dir = UP;
+    		break;
+    	case 's':
+    		dir = DOWN;
+    		break;
+    	case 'x':
+    		gameover = true;
+    		break;
 
+
+    }
 }
 
 void Logic(){
